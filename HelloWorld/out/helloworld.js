31e9b3f47c7cfff123b3505d24782d7bd31e5baa
Raul de la Cruz <rcruz2@mapfre.com>
2022-09-06 19:58:51 +0200

    New: tsconfig & refactor the code with src

--- HelloWorld/out/helloworld.js
+++
@@ -1,2 +0,0 @@
-var message = "Hello Raul!";
-console.log(message);
---
+++ src/First.ts
@@ -0,0 +1,42 @@
+
+interface Person {
+    firstName: string,
+    lastName: string,
+    job?: job, // Optional
+    isVisitor?: boolean //Optional
+}
+
+type job = 'Engineer' | 'Programmer' 
+// | = OR
+
+function generateEmail(person: Person, force?: boolean): string | undefined{
+    /* El compilador es capaz de deducir el retorno, no es obligatorio especificarlo
+       Si se especifica el retorno deben coincidir con el tipo retornado si no error */
+    // force es un parametro opcional
+    if (person.isVisitor && !force){
+        return undefined
+    } else {
+        return `${person.firstName}.${person.lastName}@email.com`
+    }
+}
+
+function isPerson(potencialPerson: any): boolean{
+    return (('firstName' in potencialPerson) && ('lastName' in potencialPerson)) 
+}
+
+function printEmailIfPerson(potencialPerson:any): void {
+    if (isPerson(potencialPerson)) {
+        console.log(generateEmail(potencialPerson))
+    } else {
+        console.log('Input is not a person')
+    }
+}
+
+printEmailIfPerson({
+    firstName: 'John',
+    lastName: 'Doe'
+})
+
+async function someAsync() {
+    return 'async'
+}
