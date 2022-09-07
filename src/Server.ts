a8fe29d56b227913fa82a4eba9e14411728663b3
Raul de la Cruz <rcruz2@mapfre.com>
2022-09-07 14:49:13 +0200

    New: abstract classes

--- src/Server.ts
+++ src/Server.ts
@@ -1,8 +1,8 @@
 
-class Server {
+abstract class BaseServer {
 
-    port: number;
-    address: string;
+    protected port: number;
+    protected address: string;
 
     constructor(port: number, address: string){
         this.port = port;
@@ -12,7 +12,21 @@ class Server {
     startServer(){
         console.log(`Starting server at: ${this.address}: ${this.port}`)
     }
+
+    abstract stopServer(): void
 }
 
-const someServer = new Server(8080, 'localhost');
+class DbServer extends BaseServer{
+
+    stopServer(): void {
+        console.log('Stopping db server')
+    }
+    constructor(port: number, address: string){
+        super(port, address);
+        console.log('Calling db server constructor')
+    }
+}
+
+const someServer = new DbServer(8080, 'localhost');
 someServer.startServer();
+someServer.stopServer();
